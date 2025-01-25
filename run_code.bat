05d8fde0ce083f68ff63690f42ce0e2fc1192b3b
van-artist <shangliu385@gmail.com>
2025-01-25 15:04:28 +0800

    更新 run_code.bat 脚本以使用 conda.exe 初始化环境并激活 nlp_env

--- run_code.bat
+++ run_code.bat
@@ -2,7 +2,8 @@
 cls
 echo [INFO] Activating environment and running code...
 
-call "%~dp0miniconda\condabin\conda.bat" activate nlp_env
+call "%~dp0miniconda\Scripts\conda.exe" init
+call "%~dp0miniconda\Scripts\conda.exe" activate nlp_env
 
 if not exist "%~dp0src\main.py" (
     echo [ERROR] No main.py found in the "src" folder. Please ensure it exists and try again.
